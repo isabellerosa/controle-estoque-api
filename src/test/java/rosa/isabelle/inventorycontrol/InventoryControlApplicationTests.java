692e5ae4e2499f74705877ed455b160895c84ea9
Isabelle Rosa <ibell.pereira@gmail.com>
2019-11-13 18:11:36 -0300

    first commit health check

---
+++ src/test/java/rosa/isabelle/inventorycontrol/InventoryControlApplicationTests.java
@@ -0,0 +1,13 @@
+package rosa.isabelle.inventorycontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
